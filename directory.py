ab6517b337908455c33a57c61aa25ab4097dd09e
Darren M <aeneascorrupt@gmail.com>
2012-07-24 23:21:02 -0700

    Added Directory helper class
    Is able to hold names. Can do the following:
    * add
    * contains

---
+++ directory.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+class Directory:
+
+	def __init__(self):
+		self.directory = {}
+
+	"""
+	"""
+	def add(self, name):
+		nameParts = name.split(' ')
+		if len(nameParts) < 2:
+			return False
+		
+		lastName = nameParts[len(nameParts) - 1].lower()
+		
+		# First occurrance of family name
+		if lastName not in self.directory:
+			self.directory[lastName] = []
+
+		firstName = nameParts[0].lower()
+		# Add first name to family
+		self.directory[lastName].append(firstName)
+		
+		return True
+
+	"""
+	"""
+	def contains(self, name):
+		nameParts = name.split(' ')
+		
+		# Only given the family name
+		if len(nameParts) == 1:
+			return nameParts[0] in self.directory
+		# Given first and last name
+		elif len(nameParts) >= 2:
+			firstName = nameParts[0].lower()
+			lastName = nameParts[len(nameParts) - 1].lower()
+
+			if lastName in self.directory:
+				return firstName in self.directory[lastName]
+
+		return False
+
+
+	def getDirectory(self):
+		return self.directory
+		
