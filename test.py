2281cd2b657a7581fa9d30296238f2591e178f76
Darren M <aeneascorrupt@gmail.com>
2012-08-13 22:31:32 -0700

    Added proper test module

---
+++ test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import logging
+
+from models.drive import Drive
+
+def main():
+    drive = Drive()
+    
+    spreadsheets = drive.list_spreadsheets()
+    
+    worksheets = drive.list_worksheets('Matsumoto Family Directory')
+    
+    cells = drive.get_data('Matsumoto Family Directory', 'Current')
+    logging.info(cells)
+
+if __name__ == "__main__":
+    main()
