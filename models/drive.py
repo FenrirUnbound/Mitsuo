2092dbf1faaeefe2b804b6a33fc3c598377a689f
Darren M <aeneascorrupt@gmail.com>
2012-08-13 22:24:12 -0700

    Added documentation to the Drive class

--- models/drive.py
+++ models/drive.py
@@ -7,50 +7,98 @@ from lib.my_user import User
 
 
 class Drive:
-    """
+    """A wrapper class for the GDocs Python Client.
+    
+    Although it does limit the client's uses and implementation uses, it
+    simplifies the feature-set for the project at hand.
+
     """
 
     def __init__(self):
-        """
-        """
+        """Base constructor.
         
+        Mainly used to initiate the client login and maintain the session.
+        
+        WARNING: If the session expires before the completion of this program,
+        you're going to have a bad time.
+            1. A session is apx 24 hours
+            2. If a session expires before this program terminates, then it
+               is getting stuck somewhere else during its operation.
+
+        """
         self._user = User()
         self._client = gdata.spreadsheet.service.SpreadsheetsService()
 
         self._client.ClientLogin(self._user.email, self._user.ticket)
 
-    def list_spreadsheets(self):
+    def get_data(self, spreadsheet, worksheet):
+        """Obtain all the cell data within a spreadsheet
+        
         """
+        result = {}
+
+        # Traverse spreadsheets tree
+
+        spreadsheets_feed = self._client.GetSpreadsheetsFeed()
+        index = self._search_feed(spreadsheet, spreadsheets_feed)
+        # spreadsheet not found
+        if(index < 0):
+            return result
+
+        id_parts = spreadsheets_feed.entry[index].id.text.split('/')
+        spreadsheet_id = id_parts[len(id_parts) - 1]
+        
+        # Traverse worksheets tree
+        
+        worksheets_feed = self._client.GetWorksheetsFeed(spreadsheet_id)
+        index = self._search_feed(worksheet, worksheets_feed)
+        # worksheet not found
+        if(index < 0):
+            return result
+
+        id_parts = worksheets_feed.entry[index].id.text.split('/')
+        worksheet_id = id_parts[len(id_parts) - 1]
+        
+        cells_feed = self._client.GetCellsFeed(spreadsheet_id, worksheet_id)
+        return cells_feed
+
+        return result
+
+    def list_spreadsheets(self):
+        """Get the list of accessible spreadsheets
+
         """
         result = []
         feed = self._client.GetSpreadsheetsFeed()
         
         return self._feed_content(feed)
 
+    # This can be improved
+    ## Possible "if string in other_string" instead of for-loop
     def list_worksheets(self, spreadsheet):
+        """Get the list of worksheets within a spreadsheet
+        
+        If the spreadsheet isn't accessible by the client, then an empty list
+        is returned. 
+        
         """
-        """
-        index = -1
-
         spreadsheets_feed = self._client.GetSpreadsheetsFeed()
-        equalized_string = self._equalize_string(spreadsheet)
-        for element in self._feed_content(spreadsheets_feed):
-            ++index
+        index = self._search_feed(spreadsheet, spreadsheets_feed)
 
-            # Found the requested spreadsheet
-            if equalized_string == self._equalize_string(element):
-                # Grab the ID
-                id_parts = spreadsheets_feed.entry[index].id.text.split('/')
-                id_ = id_parts[len(id_parts) - 1]
+        if index > 0:
+            # Grab the ID
+            id_parts = spreadsheets_feed.entry[index].id.text.split('/')
+            id_ = id_parts[len(id_parts) - 1]
 
-                worksheets_feed = self._client.GetWorksheetsFeed(id_)
+            worksheets_feed = self._client.GetWorksheetsFeed(id_)
                 
-                return self._feed_content(worksheets_feed)
+            return self._feed_content(worksheets_feed)
 
         return []
 
     def _equalize_array(self, string_array):
-        """
+        """Equalize all strings in an array
+
         """
         result = []
 
@@ -60,16 +108,42 @@ class Drive:
         return result
 
     def _equalize_string(self, string):
-        """
+        """Equalize a string
+        
+        To treat all strings (nearly) equally, it is changed to lower-case
+        characters, then is stripped of all whitespace (including spaces
+        between words).
+        
+        TODO: Strip special characters.
+                * Simplifies future implementation
+                * Assists in (highly potential) typos
+
         """
         return string.lower().replace(' ', '')
 
     def _feed_content(self, feed):
-        """
+        """Obtain the content text of a given feed
+
         """
         result = []
-        
+
         for item in feed.entry:
             result.append(item.content.text)
-        
+
         return result
+
+    def _search_feed(self, title, feed):
+        """Search for a particular text within a feed
+
+        """
+        index = -1
+
+        content = self._feed_content(feed)
+        target = self._equalize_string(title)
+        for element in content:
+            index += 1
+
+            if target == self._equalize_string(element):
+                return index
+
+        return -1
